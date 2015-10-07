16b96f59a433e46e7aa37dd1cdd94cc5a867aec6
Axel Oehmichen <axelfrancois.oehmichen11@imperial.ac.uk>
2015-10-07 15:28:11 +0000

    Proper formatting of the files

--- web-app/Scripts/eae/executeSparkJob.sh
+++ web-app/Scripts/eae/executeSparkJob.sh
@@ -1,1 +1,4 @@
-#!/usr/bin/env bash#We trigger the Spark Jobpdsh -w ssh:centos@$2 "spark-submit --py-files CrossValidation.zip --master yarn-cluster  --num-executors 2 --driver-memory 1024m  --executor-memory 512m   --executor-cores 1 $1 &"
+#!/usr/bin/env bash
+
+#We trigger the Spark Job
+pdsh -w ssh:centos@$2 "spark-submit --py-files CrossValidation.zip --master yarn-cluster  --num-executors 2 --driver-memory 1024m  --executor-memory 512m   --executor-cores 1 $1 &"
--- web-app/Scripts/eae/transferToHDFS.sh
+++ web-app/Scripts/eae/transferToHDFS.sh
@@ -1,1 +1,10 @@
-#!/usr/bin/env bash# We copy the file to the Spark masterscp $1 centos@$3:/tmp/$2# We remotly execute the hadoop putpdsh -w ssh:centos@$3 -l hdfs "hadoop fs -put /tmp/$2"# We clean up the remote temp filepdsh -w ssh:centos@$3 "rm -rf /tmp/$2"
+#!/usr/bin/env bash
+
+# We copy the file to the Spark master
+scp $1 centos@$3:/tmp/$2
+
+# We remotly execute the hadoop put
+pdsh -w ssh:centos@$3 "hadoop fs -put /tmp/$2"
+
+# We clean up the remote temp file
+pdsh -w ssh:centos@$3 "rm -rf /tmp/$2"
