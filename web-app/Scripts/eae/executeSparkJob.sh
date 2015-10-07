efd7c0771cbf9a7abcbd8511842e92e61b5922cd
Axel Oehmichen <axelfrancois.oehmichen11@imperial.ac.uk>
2015-10-07 16:45:05 +0000

    Changes to spark submit file

--- web-app/Scripts/eae/executeSparkJob.sh
+++ web-app/Scripts/eae/executeSparkJob.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
 
+args=("$@")
+STR=""
+for i in $(seq 1 $#)
+ do
+  STR+=" ${args[$i]}"
+ done
+
 #We trigger the Spark Job
-pdsh -w ssh:centos@$2 "spark-submit --py-files CrossValidation.zip --master yarn-cluster  --num-executors 2 --driver-memory 1024m  --executor-memory 512m   --executor-cores 1 $1 &"
+pdsh -w ssh:centos@$1 "spark-submit --py-files CrossValidation.zip --master yarn-cluster  --num-executors 2 --driver-memory 1024m  --executor-memory 512m   --executor-cores 1 $STR &"
