b4ba5a717ad1ae2aac0ae1cc73ec7224d0c9d48b
Axel Oehmichen <axelfrancois.oehmichen11@imperial.ac.uk>
2016-07-21 17:09:00 +0100

    ficing the eaeGrailsName

--- grails-app/conf/eAEResources.groovy
+++ grails-app/conf/eAEResources.groovy
@@ -1,10 +1,10 @@
 modules = {
     global {
-        resource url: [plugin: 'eae', dir: 'js/resource', file: 'jquery-2.1.4.min.js']
-        resource url: [plugin: 'eae', dir: 'js/resource', file: 'polyfill.min.js']
-        resource url: [plugin: 'eae', dir: 'js/resource', file: 'd3.min.js']
-        resource url: [plugin: 'eae', dir: 'js/smartR', file: 'smartR-compiled.js']
-        resource url: [plugin: 'eae', dir: 'css', file: 'smartR.css']
+        resource url: [plugin: 'eAE', dir: 'js/resource', file: 'jquery-2.1.4.min.js']
+        resource url: [plugin: 'eAE', dir: 'js/resource', file: 'polyfill.min.js']
+        resource url: [plugin: 'eAE', dir: 'js/resource', file: 'd3.min.js']
+        resource url: [plugin: 'eAE', dir: 'js/smartR', file: 'smartR-compiled.js']
+        resource url: [plugin: 'eAE', dir: 'css', file: 'smartR.css']
     }
 
 }
