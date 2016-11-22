1673ac69edcd0846def23850cb5ee856f6a2a0a7
Axel Oehmichen <axelfrancois.oehmichen11@imperial.ac.uk>
2016-11-22 01:16:34 +0000

    cleanup no longer existing resources

--- grails-app/conf/eAEResources.groovy
+++ grails-app/conf/eAEResources.groovy
@@ -3,7 +3,6 @@ modules = {
         resource url: [plugin: 'eAE', dir: 'js/resource', file: 'jquery-2.1.4.min.js']
         resource url: [plugin: 'eAE', dir: 'js/resource', file: 'polyfill.min.js']
         resource url: [plugin: 'eAE', dir: 'js/resource', file: 'd3.min.js']
-        resource url: [plugin: 'eAE', dir: 'js/smartR', file: 'smartR-compiled.js']
         resource url: [plugin: 'eAE', dir: 'css', file: 'smartR.css']
     }
 
